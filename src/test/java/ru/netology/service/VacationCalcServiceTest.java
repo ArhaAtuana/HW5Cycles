595f09892436a5b8fc94a58c3aa4d88868a9ee35
Ksenia Krestsova <arhaatuana@gmail.com>
2025-12-07 01:24:42 +0200

    paramiterized tasts added

--- src/test/java/ru/netology/service/VacationCalcServiceTest.java
+++ src/test/java/ru/netology/service/VacationCalcServiceTest.java
@@ -2,21 +2,29 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationCalcServiceTest {
 
-    @Test
-    public void shouldCalcMax() {
+    @ParameterizedTest
+  /*  @CsvSource({
+           "2, 100000, 60000, 150000",
+           "3, 10000, 3000, 20000"
+    })*/
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void shouldCalcMax(int expected, int income, int expenses, int threshold) {
 
         VacationCalcService service = new VacationCalcService();
 
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        //int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+    /*@Test
     public void shouldCalcMin() {
 
         VacationCalcService service = new VacationCalcService();
@@ -25,5 +33,5 @@ public class VacationCalcServiceTest {
         int actual = service.calculate(10_000, 3_000, 20_000);
 
         Assertions.assertEquals(expected, actual);
-    }
+    }*/
 }
